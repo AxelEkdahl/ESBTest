ae54e669897da1ba0ee23ec69075af3f5d5ad40e
AxelEkdahl <axel_93_ekdahl@hotmail.com>
2016-10-29 15:37:41 +0200

    first commit 2

---
+++ ESBTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESBTest.Startup))]
+namespace ESBTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
